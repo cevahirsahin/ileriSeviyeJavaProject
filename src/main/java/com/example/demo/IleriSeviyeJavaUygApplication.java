21f6e2534deb7c53c344d007ceddd54670276aeb
Senanur Öztürk <sena20032015@hotmail.com>
2024-10-15 15:51:32 +0300

    Updated

---
+++ src/main/java/com/example/demo/IleriSeviyeJavaUygApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IleriSeviyeJavaUygApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IleriSeviyeJavaUygApplication.class, args);
+	}
+
+}
